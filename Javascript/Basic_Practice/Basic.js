8aaa6b8c9abe36505636a59668e7ebca3c8fe8fc
unknown <nazeerahmedofficial1.com>
2022-01-30 17:52:53 +0500

    topic listed

---
+++ Javascript/Basic_Practice/Basic.js
@@ -0,0 +1,11 @@
+ /*
+ Topics:
+    1-Switch Statement
+    2-for/in and for/of
+    3-Math, Comparison , and logical operators
+    4-Exceptions , error types , data types
+    5-this keyword
+    6-Spread operator
+
+
+ */
