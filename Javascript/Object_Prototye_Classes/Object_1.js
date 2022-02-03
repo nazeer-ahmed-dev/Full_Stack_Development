72ef80676bd0bb57f53e37a1e8232b7202bfdd71
unknown <nazeerahmedofficial1.com>
2022-02-03 16:08:02 +0500

    object properties added

--- Javascript/Object_Prototye_Classes/Object_1.js
+++ Javascript/Object_Prototye_Classes/Object_1.js
@@ -59,3 +59,49 @@ let nazeer = new Person("nazeer","ahmed",20);
 let umair = new Person("umair","ahmeed",22)
 
 console.log(nazeer)
+
+
+// properties of object
+let object_properties = {
+     firstName : "Nazeer",
+     lastName : "Ahmed",
+     desc : {city : "mithi"},
+     age  : 20,
+     isAdult : function (){
+        return (this.age>20);
+    }
+}
+
+// using bracket notation
+console.log(object_properties['firstName'])
+object_properties['hairColor'] = "brown"
+
+//modifying object properties
+Object.defineProperty(object_properties,'firstName',{writable : false})
+//if you change name it will not modify
+    // object_properties.firstName = "nazeerahmed" : gives error here
+// getting property Description
+// if your desc.city then it will change but desc will not change if its writable property is false
+console.log(Object.getOwnPropertyDescriptor(object_properties,'firstName'))
+
+// enumrable property
+Object.defineProperty(object_properties,'firstName',{enumerable : false})
+for (let key in object_properties)
+{
+    console.log(object_properties[key]) // name will not print b/c its enumrable property is false
+}
+
+
+// getter and setter 
+Object.defineProperty(object_properties,'fullname',{
+    get : function(){
+        return this.firstName + " "+this.lastName
+    },
+    set : function(value){
+        var namePart =  value.split(' ');
+        this.firstName = namePart[0];
+        this.lastName = namePart[1];
+    }
+})
+console.log(object_properties.fullname)
+console.log(object_properties.fullname)
