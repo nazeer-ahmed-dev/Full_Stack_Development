14c2cfaf002e8a92ab6e6a286923c724ba0d2374
unknown <nazeerahmedofficial1.com>
2022-02-04 00:05:54 +0500

    String literal added

---
+++ Javascript/ES6+/index.js
@@ -0,0 +1,15 @@
+//String Literal : using backtick 
+    let firstName = 'Nazeer';
+    let lastName = "Ahmed"
+
+    const fullName  = `${firstName} ${lastName}`;
+    console.log(fullName)
+
+    // space using String Literal
+
+    let  fullName_1 = `${firstName}
+        ${lastName}
+        `;
+    console.log(fullName_1)
+
+// Destructuring Object
