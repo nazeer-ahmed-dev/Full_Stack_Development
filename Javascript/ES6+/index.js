236485ee23abaf0a284d0f44e12e964f9849fca5
unknown <nazeerahmedofficial1.com>
2022-02-04 10:11:08 +0500

    spread operator and for/of loop added

--- Javascript/ES6+/index.js
+++ Javascript/ES6+/index.js
@@ -11,7 +11,7 @@
         ${lastName}
         `;
     console.log(fullName_1)
-
+    
 // Destructuring Object
 const player = {
     name : 'Nazeer ahmed',
@@ -23,3 +23,93 @@ const player = {
 }
 const {name,qulification,address:{city}} = player
 console.log(`${name} has done ${qulification} and he live in  ${city}`)
+
+//Destructuring Array
+let [fName,lName] =  ['Nazeer','ahmed'];
+lName = "Ahmed"
+console.log(firstName + " "+lastName)
+
+//Object Literal
+function addressMaker(city,state)
+{
+    const newAddress = {city,state};
+    console.log(newAddress);
+}
+addressMaker('Mithi','Mithi_1')
+
+// example of object literal
+function addressMaker_1(address)
+{
+    const { city, state} = address; // By chaning name of city and state you will get undefined
+    const newAddress_1 = {
+
+        city,state,
+        country : 'Pakistan'
+    }
+    console.log(newAddress_1)
+}
+addressMaker_1({city:'Mithi',state:'MithI_1'})
+
+// For/of Loop
+let incomes = [1232,2324,3535];
+let total = 0;
+
+for (const income in incomes)
+{
+    total += income;
+}
+console.log(total)
+// example of for/of loop
+let fullName_2 = "Nazeer ahmed"
+for(const char of fullName_2)
+{
+    console.log(char)
+}
+//example 2
+
+const student =[
+    {name:'Nazeer',city:'Mithi'},
+    {name:'Umair',city:'Ratodero'}
+];
+
+for(const std of student)
+{
+    console.log(std)
+    for(const data in std)
+    {
+        console.log(std[data])
+    }
+}
+
+// spread operator on array
+let contacts = ["Nazeer","umair","Haseeb"]
+
+let personalFriend = contacts
+contacts.push("ahmed") // it walo also reflect the personalFriend 
+let personalFriend_1 = [...contacts]
+contacts.push("qudoos")
+console.log(contacts)
+console.log(personalFriend)
+console.log(personalFriend_1)
+
+//example
+let array = [...personalFriend,"add1"]
+console.log(array)
+
+// spread operator on object
+ let product=[{
+    "productId" : 24,
+    "item " : 2,
+    "name" : "LUX soap",
+    "price " : 100,
+    calculatePrice : function (){
+        return (this["item "]-this["price "]);
+    }
+}]
+const product2 = {...product[0]}
+
+product2.name = "CHNAGE"
+console.log(product)
+console.log(product2)
+
+// Rest Operator
