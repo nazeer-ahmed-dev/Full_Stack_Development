902d467892eaa2a01615e5bf046cd3c5531bb1b4
unknown <nazeerahmedofficial1.com>
2022-02-24 12:19:37 +0500

    backend completed

---
+++ CRUD_Project_API/backend/controller/user.js
@@ -0,0 +1,46 @@
+import { v4 as uuidv4 } from 'uuid';
+const users = []
+
+export const createUser = (req,res)=>{
+    const user = req.body
+    const userid= uuidv4()
+ 
+    const userWid = {...user,id:userid}
+     users.push(userWid)
+ 
+     res.send("User added")
+ }
+
+ export const getUser = (req,res)=>{
+
+    
+    const {id} = req.params
+    //res.send(id)
+    res.send(users.find((user) => user.id===id))
+}
+
+export const deleteUser = (req,res)=>{
+    const {id} = req.params
+    var index = users.map(x => {
+        return x.id;
+      }).indexOf(id);
+      
+      users.splice(index, 1);
+      res.send(users)
+}
+
+export const updateUser = (req,res)=>{
+    const {id} = req.params;
+    const {firstName,lastName,age} = req.body
+    const user = users.find((users1)=>users1.id===id)
+    if(firstName)  user.firstName  = firstName
+    if(lastName)  user.lastName = lastName;
+    if(age) user.age = age
+
+    res.send("user updated")
+}
+
+export const main1 = (req,res)=>{
+    console.log(users)
+    res.send(users)
+}
--- CRUD_Project_API/backend/routes/user.js
+++ CRUD_Project_API/backend/routes/user.js
@@ -1,44 +1,17 @@
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
+import { createUser,getUser ,deleteUser,updateUser,main1} from '../controller/user.js';
 const router = express.Router();
 
-const users = [
-    
-]
+
 
 // all routes in here are starting with / users
-router.get('/',(req,res)=>{
-    console.log(users)
-    res.send(users)
-})
-
+router.get('/',main1)
 // adding data
-router.post('/',(req,res)=>{
-   const user = req.body
-   const userid= uuidv4()
-
-   const userWid = {...user,id:userid}
-    users.push(userWid)
-
-    res.send("User added")
-})
-
-router.get('/:id',(req,res)=>{
-
-    
-    const {id} = req.params
-    //res.send(id)
-    res.send(users.find((user) => user.id===id))
-})
+router.post('/',createUser)
+router.get('/:id',getUser)
+router.delete('/:id',deleteUser)
+router.patch('/:id',updateUser)
 
 
-router.delete('/:id',(req,res)=>{
-    const {id} = req.params
-    var index = users.map(x => {
-        return x.id;
-      }).indexOf(id);
-      
-      users.splice(index, 1);
-      res.send(users)
-})
 export default router;
